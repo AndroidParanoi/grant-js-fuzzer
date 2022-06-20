490c58eacaa17a0124414362e52ebaa75b455eb7
AndroidParanoid <90028731+AndroidParanoi@users.noreply.github.com>
2022-06-20 01:59:37 +0100

    Update jsfuzz.py

--- jsfuzz.py
+++ jsfuzz.py
@@ -13,7 +13,6 @@ class JSFuzzer:
             statements = f.readlines()
             for cycle in range(cycles):
                 for statement in statements:
-                    statement = statement.rstrip()
                     if statement == "BEGIN_ONLY" and cycle != 0:
                         skip = True
                         continue
@@ -36,61 +35,63 @@ class JSFuzzer:
         loop_var = ""
         function_name = ""
         answer = ""
-        if isinstance(statement, str):
-            if "try" in statement:
-                answer += "try{"
-            if "loop" in statement:
-                loop = statement.split(",")
-                loop_var = loop[1]
-                loop_count = loop[2]
-                answer += self.do_for_loop(loop_var, loop_count)
-            if "var" in statement and "function" not in statement:
+        if not isinstance(statement, str):
+            return ""
+        if "try" in statement:
+            answer += "try{"
+        if "loop" in statement:
+            loop = statement.split(",")
+            loop_var = loop[1]
+            loop_count = loop[2]
+            answer += self.do_for_loop(loop_var, loop_count)
+        if "var" in statement and "function" not in statement:
+            var = statement.split(" ")
+            var_name = var[1]
+            var_value = var[3]
+            answer += "var "
+            answer += self.insert_value_in_variable(var_name, var_value)
+        if ("var" not in statement) and ("=" in statement) and ("if" not in statement and "else if" not in statement and "else" not in statement):
+            if "FCALL" not in statement:
                 var = statement.split(" ")
-                var_name = var[1]
-                var_value = var[3]
-                answer += "var "
-                answer += self.insert_value_in_variable(var_name, var_value)
-            if (("var" not in statement) and ("function" not in statement) and ("=" in statement) and "===" not in statement):
-                if "MODIFY_ITSELF_ARRAY" in statement:
-                    answer += self.change_itself_array(statement)
+                var_name = var[0]
+                if "}" in statement:
+                    var_value = " ".join(var[2:])
                 else:
-                    if "FCALL" not in statement:
-                        var = statement.split(" ")
-                        var_name = var[0]
-                        var_value = var[2]
-                        answer += self.insert_value_in_variable(var_name, var_value)
-            if "function" in statement:
-                function = statement.split(" ")
-                function_name = function[1]
-                argument = function[3] 
-                answer += self.create_function(function_name, argument)
-            if "close_bracket" in statement:
-                answer += "}"
-            if "FCALL" in statement:
-                split_statement = statement.split("=")
-                var_name = split_statement[0]
-                call_args = split_statement[1].split(",")
-                libr_call = call_args[1]
-                for function in call_args[2:]:
-                    answer += f"{var_name} = {libr_call}.{function};"
-            if "strict-mode" in statement:
-                answer += '"use-strict";'
-            if "catch" in statement:
-                answer += "}catch(e){console.log(e);}"
-            if "call" in statement:
-                answer += f"{statement.split(',')[1]};"            
-            if "if" in statement or "else if" in statement or "else" in statement or "return" in statement:
-                answer += statement
-            return answer
+                    var_value = var[2]
+                if "MODIFY_ITSELF_ARRAY" in statement:
+                    var_value = f"new Array({randint(10000,20000)});"
+                answer += self.insert_value_in_variable(var_name, var_value)
+        elif "function" in statement and "var" in statement:
+            function = statement.split(" ")
+            function_name = function[1]
+            argument = function[3] 
+            answer += self.create_function(function_name, argument)
+        if "close_bracket" in statement:
+            answer += "}"
+        if "FCALL" in statement:
+            split_statement = statement.split("=")
+            var_name = split_statement[0]
+            call_args = split_statement[1].split(",")
+            libr_call = call_args[1]
+            for function in call_args[2:]:
+                answer += f"{var_name} = {libr_call}.{function};"
+        if "strict-mode" in statement:
+            answer += '"use-strict";'
+        if "catch" in statement:
+            answer += "}catch(e){console.log(e);}"
+        if "call" in statement:
+            answer += f"{statement.split(',')[1]};"            
+        if "if" in statement or "else if" in statement or "else" in statement:
+            answer += statement
+        if "return" in statement and "}" not in statement:
+            answer += "return"
+        return answer
 
     def return_random_choices(self):
         return [["Object", "Function", "Array", "RegExp", "Datetime"], ["!==", "!==", "==", "===", "<", ">", "<=", ">=", "&&", "||"], ["undefined", "''", "true", "false", "1", "{}", "null", "[]"]]
                 
     def return_available_objects(self):
         return ["new Object", "new Function", "new String", "new Array", "new AggregateError", "new AsyncFunction", "new Atomics", "new BigInt64Array", "new BigUint64Array", "new Boolean", "new Dataview", "new Date", "new decodeURI"]
-
-    def change_itself_array(self, statement):
-        return f"{statement.split('=')[0]} = new Array({randint(1000, 10000)});"
 
     def call_function(self, function_name):
         return f"{function_name}()"
@@ -101,14 +102,8 @@ class JSFuzzer:
     def insert_value_in_variable(self, var_name, value):
         return f"{var_name} = {value}"     
 
-    def return_keywords(self):
-        return ["RANDOM_NUMBER", "LENGTH"]
-
     def do_for_loop(self, var_name, iteration_count):
         return "for(let " + var_name + " = 0;" + var_name + "<=" + iteration_count + ";" + var_name + "++){"
-
-    def fill_identifier(self, y):
-        return "".join((choice(string.ascii_letters) for _ in range(y)))
     
     #def js_run(self, func):
         #system("/home/androidparanoid/DynamoRIO-Linux-9.0.1/bin64/drrun -t drcov ")
